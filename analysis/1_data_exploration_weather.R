3692c12d79d1c91f984a515affb24c05df5105d9
wx-ecology <wenjing.xuuu@gmail.com>
2023-01-18 13:10:19 -0800

    analyzing weather data

--- analysis/1_data_exploration_weather.R
+++ analysis/1_data_exploration_weather.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(hrbrthemes)
 library(lubridate)
 library(ggplot2)
+library(patchwork)
 
 weather_sum_all <- data.frame()
 weather_all <- data.frame()
@@ -13,23 +14,41 @@ for (file in files) {
     select('Date & Time', 'Temp - _', 'Rain - mm') %>%
     rename(DateTime = 'Date & Time', Temp ='Temp - _', Rain = 'Rain - mm') %>%
     mutate(DateTime = mdy_hm(DateTime, tz = "Africa/Nairobi"),
-           Yr = year(DateTime), Mo = month(DateTime),
-           Temp = as.numeric(Temp)) 
+           Yr = year(DateTime), Mo = month(DateTime), da = day(DateTime),
+           Temp = as.numeric(Temp)) %>%
+    filter(DateTime >= mdy_hm("05-01-2018 00:00", tz = "Africa/Nairobi") & 
+             DateTime < mdy_hm("09-01-2020 00:00", tz = "Africa/Nairobi")   )
 
   weather_all <- rbind(weather, weather_all)
     
   weather_sum <- weather %>% 
     group_by(Yr, Mo) %>%
-    summarise(Total_Rain = sum(Rain, na.rm = T), Ave_Temp = mean(Temp, na.rm = T))
+    summarise(Days_Recorded = length(unique(da)),
+              Total_records = n(), 
+              Total_Rain = sum(Rain, na.rm = T), 
+              Ave_Rain = mean(Rain, na.rm = T),
+              Ave_Temp = mean(Temp, na.rm = T))
   
   weather_sum_all <- rbind(weather_sum_all, weather_sum)
 }
 
-weather_sum_all %>% mutate(Date = paste0(Yr,  "-", Mo, "-01"), 
+weather_sum_all
+
+p1 <-  weather_sum_all  %>% mutate(Date = paste0(Yr,  "-", Mo, "-01"), 
                            Date = ymd(Date)) %>% 
-  ggplot(aes(x = Date, y = Total_Rain)) +
+  ggplot(aes(x = as.factor(Mo), y = Total_Rain, group = Yr)) +
+  geom_line(aes(color = as.factor(Yr))) +
   geom_point() +
   theme_ipsum()
+
+p2 <- weather_sum_all %>% mutate(Date = paste0(Yr,  "-", Mo, "-01"), 
+                           Date = ymd(Date)) %>% 
+  ggplot(aes(x = Date, y = Total_records)) +
+  geom_line() +
+  geom_point() +
+  theme_ipsum()
+
+p1/p2
 
 weather_sum_all %>% mutate(Date = paste0(Yr,  "-", Mo, "-01"), 
                            Date = ymd(Date)) %>% 
