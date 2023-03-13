00ade77ac10053acc0c074ea042d9a34e2fdd53b
wx-ecology <wenjing.xuuu@gmail.com>
2023-03-13 15:49:11 -0700

    march 13

--- analysis/3_spp_cooccur_data_compling.R
+++ analysis/3_spp_cooccur_data_compling.R
@@ -9,7 +9,7 @@ protein <- read_csv("./data/cleaned_grass_data.csv") %>%  arrange(Year, Month) %
   # match the animal abundance with quality measure of the month prior
   dplyr::select(Transect, Site, month_id, Protein, Avg_Height, Percent_Grazed)
 
-sample_sites <- read_csv("./data/Sampling_site.csv")  # for getting coordinates
+sample_sites <- read_csv("./data/Sampling_site.csv") %>% select(-Name)  # for getting coordinates
 
 data <- read_csv("./data/cleaned_animal_data.csv") %>% 
   mutate( Date = ymd(Date), 
@@ -32,5 +32,5 @@ data <- read_csv("./data/cleaned_animal_data.csv") %>%
           Height_lag1 = Avg_Height,
           Pgrazed = Percent_Grazed.x,
           Pgrazed_lag1 = Percent_Grazed.y)  # xxx_lag1 represents the NDVI value from the previous month at the same site
-# 1,680 × 40
+# 1,680 × 39
 write_csv(data, "./data/for-spp-relationship/mara-cooccurence-compiled.csv") 
--- analysis/4_1_HMSC.R
+++ analysis/4_1_HMSC.R
@@ -2,20 +2,59 @@
 ## 1. understand the relative contributions of environmental variables and biotic interactions to species occurrences
 ## 2. predict cattle and other species spatial use in wetter and dryer months to be used as input later soil and vegetation condition analyses
 
+library(tidyverse)
 library(Hmsc)
 library(corrplot)
 
 # -----data prep ---
+data <- read_csv("./data/for-spp-relationship/mara-cooccurence-compiled.csv") %>%
+  dplyr::select(-Date, -Northing, -Easting) %>% # this northing and easting is measured by GPS in the field with lots of noises. using site GPS locations instead.
+  mutate(Transect = factor(Transect, levels = c("C", "D", "E", "F", "G")),
+         Site = as.numeric(Site),  # distance to boundary 
+         sin_month = sin(2*pi*Month/12),
+         cos_month = cos(2*pi*Month/12)) %>% 
+  filter(Yr_Mo != "2018-05",
+         !is.na(Protein_lag1),
+         !is.na(Height_lag1)) %>%  # the first month does not have previous month measurement
+  # resulting in 1520 * 38 valid sampling units
+  mutate(sample.id = as.factor(seq(1:nrow(data))))
 
-# notes - study desigh - lognormal poisson"; 
-dristr = "lognormal poisson"
+# community data 
+Y = data %>% 
+  dplyr::select(Cattle, Wildebeest, Zebra, Thompsons_Gazelle, Impala, Topi,
+                Eland, Buffalo, Grants_Gazelle, Waterbuck, Dikdik, Elephant) %>%
+  as.matrix(.)
 
-sample.id = as.factor(1:n)
-studyDesign = dataframe(sample = sample.id,
-                        plot = as.factor(), # c1 c2 etc. but this column is not used as random effect as in ranLevels because X and Ys are used
-                         )  
+dim(Y) # 12 species in 1520 sampling units
+#S = rowSums(Y)
+#P = colMeans(Y)
 
-rownames(xycoords) = sample.id
+# environmental data 
+XData <- data %>%
+  dplyr::select(Name, Pgrazed_lag1, Precip, 
+              Protein_lag1, Height_lag1, Month)   ## <<------------------ two options. including month as random variable or as fixed effect with circular transformation 
+
+# spatial data 
+xy <- data %>%
+  dplyr::select(x, y) %>% as.matrix(.)
+rownames(xy) = data$sample.id
+colnames(xy) = c("x-coordinate","y-coordinate") 
+
+# temporal data 
+month <- data %>%
+  dplyr::select(Month) %>% as.matrix(.)
+rownames(month) = data$sample.id
+colnames(month) = "month"  ???
+# trait data 
+## ..... TrData 
+
+
+
+studyDesign = data.frame(sample = as.factor(data$sample.id),
+                         plot = as.factor(data$Name),
+                         month = as.factor()) 
+rL = HmscRandomLevel(sData = xy)  # sData defines the spatial structure 
+
 
 
 month <- as.matrix(data.frame(studyDesign$month))  ## as in serial number OR real month??
@@ -28,15 +67,15 @@ rownames(plot) <- studyDesign$Year
 # random effect 
 # include a random effect at the level of sampling unit to estimate species-to-species residual associations
 rL1 = HmscRandomLevel(units = studyDesign$sample)
-rL2 = HmscRandomLevel(sData = xycoords) # spatial structure
+rL2 = HmscRandomLevel(sData = xy) # spatial structure
 rL3 = HmscRadomLevel(sData = studyDesign$month) # temporal structure 
 ranLevels = list(rL1, rL2, rL3)
 
-rnd_eff = list("sampling_plot" =HmscRandomLevel(sData = xycoords),   # account for spatial autocorrelation
-               "year" = HmscRandomLevel(sData = study_design$year))   # account for temporal autocorrelation. (Temporal data can be modelled the same way as spatial data, but with only one dimension.
+rnd_eff = list("sampling_sites" =HmscRandomLevel(sData = xycoords),   # account for spatial autocorrelation
+               "month" = HmscRandomLevel(sData = study_design$year))   # account for temporal autocorrelation. (Temporal data can be modelled the same way as spatial data, but with only one dimension.
 
 
-
+XFormula = ~x1+x2
 
 
 
@@ -45,6 +84,7 @@ rnd_eff = list("sampling_plot" =HmscRandomLevel(sData = xycoords),   # account f
 
 
 #  ----- model construction -----
+dristr = "lognormal poisson"
 m = Hmsc(Y = Y, XData = XData, XFormula = ~x1+x2)
 
 # To fit the HMSC model with Bayesian inference
