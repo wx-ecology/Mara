e5ce1efc9dc9bb16a5433a52a3d127d56e5924c6
wx-ecology <wenjing.xuuu@gmail.com>
2023-03-15 14:44:35 -0700

    HMSC run 1!

--- analysis/4_1_HMSC.R
+++ analysis/4_1_HMSC.R
@@ -8,16 +8,20 @@ library(corrplot)
 
 # -----data prep ---
 data <- read_csv("./data/for-spp-relationship/mara-cooccurence-compiled.csv") %>%
-  dplyr::select(-Date, -Northing, -Easting) %>% # this northing and easting is measured by GPS in the field with lots of noises. using site GPS locations instead.
-  mutate(Transect = factor(Transect, levels = c("C", "D", "E", "F", "G")),
-         Site = as.numeric(Site),  # distance to boundary 
+  #dplyr::select(-Northing, -Easting) %>% # this northing and easting is measured by GPS in the field with lots of noises. using site GPS locations instead.
+  mutate(Site = as.numeric(Site),  # distance to boundary 
          sin_month = sin(2*pi*Month/12),
          cos_month = cos(2*pi*Month/12)) %>%   # ). To account for the periodic nature of months over time, we included the mean timing of each event as the linear effect of its cosine and sine transformations
   filter(Yr_Mo != "2018-05",
          !is.na(Protein_lag1),
          !is.na(Height_lag1)) %>%  # the first month does not have previous month measurement
-  # resulting in 1520 * 38 valid sampling units
-  mutate(sample.id = as.factor(seq(1:nrow(data))))
+  dplyr::select(
+    month_id, Name, x, y,  # study design info
+    Cattle, Wildebeest, Zebra, Thompsons_Gazelle, Impala, Topi,  # species counts
+    Eland, Buffalo, Grants_Gazelle, Waterbuck, Dikdik, Elephant, # species counts
+    Site, Name, Pgrazed_lag1, Precip, Protein_lag1, Height_lag1, sin_month, cos_month # environmental data
+  ) %>%
+  drop_na(.) # 1516 * 23 
 
 # community data 
 Y = data %>% 
@@ -25,98 +29,71 @@ Y = data %>%
                 Eland, Buffalo, Grants_Gazelle, Waterbuck, Dikdik, Elephant) %>%
   as.matrix(.)
 
-dim(Y) # 12 species in 1520 sampling units
+dim(Y) # 12 species in 1516 sampling units
 #S = rowSums(Y)
 #P = colMeans(Y)
 
 # environmental data 
 XData <- data %>%
-  dplyr::select(Name, Pgrazed_lag1, Precip, 
-              Protein_lag1, Height_lag1, Month)   ## <<------------------ two options. including month as random variable or as fixed effect with circular transformation 
+  dplyr::select(Site, Pgrazed_lag1, Precip, Protein_lag1, Height_lag1, sin_month, cos_month) 
 
-## random effect ## 
-# spatial data 
-xy <- data %>%
-  dplyr::select(x, y) %>% as.matrix(.)
-rownames(xy) = data$sample.id
-colnames(xy) = c("x-coordinate","y-coordinate") 
+XFormula = ~ Site + Pgrazed_lag1 + Precip + Protein_lag1 + Height_lag1 + sin_month + cos_month
 
-# study design 
-studyDesign = data.frame(sample = site=as.factor(site),year=as.factor(year),)
-
-month <- data %>%
-  dplyr::select(Month) %>% as.matrix(.)
-rownames(month) = data$sample.id
-colnames(month) = "month"  ???
-  
-  
-# trait data 
-## ..... TrData 
-
-
-
-studyDesign = data.frame(sample = as.factor(data$sample.id),
-                         plot = as.factor(data$Name),
-                         month = as.factor()) 
-rL = HmscRandomLevel(sData = xy)  # sData defines the spatial structure 
-
-
-
-month <- as.matrix(data.frame(studyDesign$month))  ## as in serial number OR real month??
-studyDesign$month <- as.factor(sprintf('month_%.3d', studyDesign$month))
-studyDesign$Sample <- NULL
-rownames(time) <- studyDesign$month
-plot <- as.matrix(data.frame(studyDesign$Plot))
-rownames(plot) <- studyDesign$Year
-
-# random effect 
+## random effect ##
 # To account for the nature of the study design and to evaluate co-variation among xxxx, 
 # we included three random effects for the site, the year and the sampling unit (that is, year–site pairs)
 # include a random effect at the level of sampling unit to estimate species-to-species residual associations
-rL1 = HmscRandomLevel(units = studyDesign$sample)
-rL2 = HmscRandomLevel(sData = xy) # spatial structure
-rL3 = HmscRadomLevel(sData = studyDesign$month) # temporal structure 
-ranLevels = list(rL1, rL2, rL3)
 
-rnd_eff = list("sampling_sites" =HmscRandomLevel(sData = xycoords),   # account for spatial autocorrelation
-               "month" = HmscRandomLevel(sData = study_design$year))   # account for temporal autocorrelation. (Temporal data can be modelled the same way as spatial data, but with only one dimension.
+# study design 
+studyDesign = data.frame(sample = as.factor(seq(1:nrow(data))), 
+                         plot = as.factor(data$Name),
+                         month = as.factor(data$month_id))  # account for repeated measurement in different sampling seasons, the month should be a serial number month to indicate different survey season
+
+# spatial data 
+xy <- data %>%
+  dplyr::select(x, y) %>% as.matrix(.)
+rownames(xy) = studyDesign$plot
+colnames(xy) = c("x-coordinate","y-coordinate") 
+
+# temporal data 
+month <- data %>%
+  dplyr::select(month_id) %>% as.matrix(.)
+rownames(month) <- studyDesign$month
+colnames(month) <- "month"
+
+rL.sample = HmscRandomLevel(units = levels(studyDesign$sample)) # to obtain residual correlations; units = ... degines an unstructured random effect; 
+rL.temporal = HmscRandomLevel(sData = unique(month))  # sData defines the autocorrelation structure, temporal autocorrelation
+rL.spatial = HmscRandomLevel(sData = unique(xy))  # spatial autocorrelation; also used to account for site effects
+
+ranLevels = list(sample = rL.sample, month = rL.temporal, plot = rL.spatial)
+
+# trait data 
+# to be comparable with ecoCopula, not including body mass here but should include in the consequence analysis 
+
+#  ----- model construction and fitting with Bayesian inference -----
+# Fitting the four alternative HMSC models with increasing thinning
+samples = 25
+nChains = 4
+ModelDir = "./results/Hmsc_model/"
+for (thin in c(1,10,100,1000)){
+  m = Hmsc(Y=Y, XData = XData,  XFormula = XFormula,
+           #  TrData = TrData,TrFormula = TrFormula,
+           distr = "lognormal poisson",
+           studyDesign = studyDesign, 
+           ranLevels = ranLevels)
+  m = sampleMcmc(m, samples = samples, thin=thin,
+                 adaptNf=rep(ceiling(0.4*samples*thin),m$nr), 
+                 transient = ceiling(0.5*samples*thin),
+                 nChains = nChains)
+  filename = file.path(ModelDir, paste("model_thin_", as.character(thin),
+                                       "_samples_", as.character(samples),
+                                       "_chains_",as.character(nChains),
+                                       ".Rdata",sep = ""))
+  save(m,file=filename)
+}
 
 
-XFormula = ~x1+x2
-
-
-
-
-
-
-
-#  ----- model construction -----
-dristr = "lognormal poisson"
-m = Hmsc(Y = Y, XData = XData, XFormula = ~x1+x2)
-
-# To fit the HMSC model with Bayesian inference
-# Set parameter 
-nChains = 2
-test.run = FALSE  # <- only small amount of MCMC sampling
-if (test.run){
-  #with this option, the vignette runs fast but results are not reliable
-  thin = 1
-  samples = 10
-  transient = 5
-  verbose = 0
-} else {
-  #with this option, the vignette evaluates slow but it reproduces the results of the #.pdf version
-  thin = 10
-  samples = 1000
-  transient = 500*thin
-  verbose = 0 }
-
-# call sampleMcmc and thus estimate the model parameters.
-m = sampleMcmc(m, thin = thin, samples = samples, transient = transient, nChains = nChains, nParallel = nChains, verbose = verbose)
-
-# ------ examining MCMC convergence -------
-
-###
+# ------ evaluating model convergence -------- #
 # In a model with random effects, it is important to look at the convergence diagnostics not only for the
 # β parameters, but also for the Ω parameters. The matrix Ω is the matrix of species-to-species residual
 # covariances.
